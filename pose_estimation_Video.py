359c91f833987c40e2e12f2dccbb7300334b9f2f
Nikita Udodenko <nikita.udo13@gmail.com>
2022-10-10 18:41:33 -0500

    BODY_25 model added

--- pose_estimation_Video.py
+++ pose_estimation_Video.py
@@ -1,7 +1,5 @@
 import cv2
 import time
-
-from traitlets import default
 import parser
 
 # select if its front or side 
@@ -11,38 +9,57 @@ class Skeleton:
         self.device = device
         self.thres = thres
 
-        match model:
-            case "MPI": # requires higher threshold
-                print("Using MPI model")
-                self.BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-                            "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-                            "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "Chest": 14, "Background": 15 }
+        if model == "BODY_25":
+            print("Using BODY_25 model")
+            self.BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                        "LShoulder": 5, "LElbow": 6, "LWrist": 7, "MidHip": 8, "RHip": 9, "RKnee": 10,
+                        "RAnkle": 11, "LHip": 12, "LKnee": 13, "LAnkle": 14, "REye": 15,
+                        "LEye": 16, "REar": 17, "LEar": 18, "LBigToe": 19, "LSmallToe": 20, "LHeel": 21, 
+                        "RBigToe": 22, "RSmallToe": 23, "RHeel": 24, "Background": 25 }
 
-                self.POSE_PAIRS = [ ["Head", "Neck"], ["Neck", "RShoulder"], ["RShoulder", "RElbow"],
-                                    ["RElbow", "RWrist"], ["Neck", "LShoulder"], ["LShoulder", "LElbow"],
-                                    ["LElbow", "LWrist"], ["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"],
-                                    ["RKnee", "RAnkle"], ["Chest", "LHip"], ["LHip", "LKnee"], ["LKnee", "LAnkle"] ]
+            self.POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+                        ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+                        ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+                        ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+                        ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"], ["LAnkle", "LBigToe"],
+                        ["LAnkle", "LSmallToe"], ["LAnkle", "LHeel"], ["RAnkle", "RBigToe"], ["RAnkle", "RSmallToe"],
+                        ["RAnkle", "RHeel"] ]
 
-                protoFile_mpi = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
-                weightsFile_mpi = "pose/mpi/pose_iter_160000.caffemodel"
-                self.net = cv2.dnn.readNetFromCaffe(protoFile_mpi, weightsFile_mpi)
+            protoFile_body_25 = "pose/body_25/pose_deploy.prototxt"
+            weightsFile_body_25 = "pose/body_25/pose_iter_584000.caffemodel"
+            self.net = cv2.dnn.readNetFromCaffe(protoFile_body_25, weightsFile_body_25)
 
-            case default: # COCO is default model
-                print("Using COCO model")
-                self.BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-                            "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-                            "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-                            "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+        elif model == "MPI": # requires higher threshold
+            print("Using MPI model")
+            self.BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                        "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                        "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "Chest": 14, "Background": 15 }
 
-                self.POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-                            ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-                            ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-                            ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-                            ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+            self.POSE_PAIRS = [ ["Head", "Neck"], ["Neck", "RShoulder"], ["RShoulder", "RElbow"],
+                                ["RElbow", "RWrist"], ["Neck", "LShoulder"], ["LShoulder", "LElbow"],
+                                ["LElbow", "LWrist"], ["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"],
+                                ["RKnee", "RAnkle"], ["Chest", "LHip"], ["LHip", "LKnee"], ["LKnee", "LAnkle"] ]
 
-                protoFile_coco = "pose/coco/pose_deploy_linevec.prototxt"
-                weightsFile_coco = "pose/coco/pose_iter_440000.caffemodel"
-                self.net = cv2.dnn.readNetFromCaffe(protoFile_coco, weightsFile_coco)
+            protoFile_mpi = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+            weightsFile_mpi = "pose/mpi/pose_iter_160000.caffemodel"
+            self.net = cv2.dnn.readNetFromCaffe(protoFile_mpi, weightsFile_mpi)
+
+        else: # COCO is default model
+            print("Using COCO model")
+            self.BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                        "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                        "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+                        "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+
+            self.POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+                        ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+                        ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+                        ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+                        ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+
+            protoFile_coco = "pose/coco/pose_deploy_linevec.prototxt"
+            weightsFile_coco = "pose/coco/pose_iter_440000.caffemodel"
+            self.net = cv2.dnn.readNetFromCaffe(protoFile_coco, weightsFile_coco)
 
         if (device == "gpu"):
             self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
@@ -108,7 +125,7 @@ class Skeleton:
                 total_fps += fps
                 frame_count += 1
 
-                cv2.imshow('pose', frame)
+                # cv2.imshow('pose', frame)
 
                 self.result.write(frame)
 
@@ -132,7 +149,7 @@ def main():
     if args.source == '0':
         args.source = 0
 
-    skeleton = Skeleton(args.source, args.device, args.model, args.thres)
+    skeleton = Skeleton(args.source, args.device, args.model)
     avg_fps = skeleton.pose_estimation()
     skeleton.release()
 
