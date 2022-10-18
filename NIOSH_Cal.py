1886437c55f9b6592010772f2efc31e2a1932f01
Anne Bell <bellanne881@gmail.com>
2022-10-18 10:47:37 -0500

    updated multipliers

--- NIOSH_Cal.py
+++ NIOSH_Cal.py
@@ -1,10 +1,10 @@
 #run just a basic calculation for the recommended weight
 def RecommendWeight (horizontialMulti,verticalMulti,DistMulti, cM):
     #set the defult values since we dont add this to our equation and its mutiplying make it 1
-    asymetricMulti = 1
-    freq = 1
+    asymetricMulti = 1.0
+    freq = 1.0
     #RWL = LC (51) x HM x VM x DM x AM x FM x CM
-    recWeight = 51 * horizontialMulti * verticalMulti * DistMulti * cM * freq * asymetricMulti
+    recWeight = 51.0 * horizontialMulti * verticalMulti * DistMulti * cM * freq * asymetricMulti
     
     return recWeight
 
@@ -25,17 +25,17 @@ def HMFactor(horizontialDist):
 
 #run the vertical distance to get the VM factor
 def VMFactor(verticalDist):
-    if(verticalDist <= 0 and verticalDist < (30 * 2.54 )):
+    if(verticalDist >= 0 and verticalDist < (30 * 2.54 )):
         return 1.0
-    elif (verticalDist <= 30 and verticalDist < (50 * 2.54 )):
+    elif (verticalDist >= 30 and verticalDist < (50 * 2.54 )):
         return 0.87
-    elif (verticalDist <= 50 and verticalDist < (70 * 2.54 )):
+    elif (verticalDist >= 50 and verticalDist < (70 * 2.54 )):
         return 0.93
-    elif (verticalDist <= 70 and verticalDist < (100 * 2.54 )):
+    elif (verticalDist >= 70 and verticalDist < (100 * 2.54 )):
         return 0.99
-    elif (verticalDist <= 100 and verticalDist < (150 * 2.54 )):
+    elif (verticalDist >= 100 and verticalDist < (150 * 2.54 )):
         return 0.93
-    elif (verticalDist <= 150 and verticalDist < (175 * 2.54 )):
+    elif (verticalDist >= 150 and verticalDist < (175 * 2.54 )):
         return 0.78
     elif (verticalDist == 175):
         return 0.70
@@ -43,37 +43,42 @@ def VMFactor(verticalDist):
         return 0
 
 #run the distance the load travels to get the DM factor
-def DMFactor(dist):
-    if(dist <= 0 and dist >= (25 * 2.54 )):
+def DMFactor(distTravel):
+    if(distTravel >= 0 and distTravel <= (25 * 2.54 )):
         return 1.0
-    elif (dist < 25 and dist >= (40 * 2.54 )):
+    elif (distTravel > 25 and distTravel <= (40 * 2.54 )):
         return 0.93
-    elif (dist < 40 and dist >= (55 * 2.54 )):
+    elif (distTravel > 40 and distTravel <= (55 * 2.54 )):
         return 0.90
-    elif (dist < 55 and dist >= (100 * 2.54 )):
+    elif (distTravel > 55 and distTravel <= (100 * 2.54 )):
         return 0.87
-    elif (dist < 100 and dist >= (175 * 2.54 )):
+    elif (distTravel > 100 and distTravel <= (175 * 2.54 )):
         return 0.85
-    elif (dist > 175):
-        return 0
+    elif (distTravel > 175):
+        return 0.0
     
     
 
 #run to equation to couplingMuti from user input into a number
 def couplingMultiplier(CM):
     if CM == "Good":
-        return 1
+        return 1.0
     elif CM == "Fair":
-        return 2
+        return 2.0
     elif CM == "Poor":
-        return 3
+        return 3.0
 
 #run for the  lifting index 
-def liftingIndex (horizontialMulti,verticalMulti,DistMulti, cM, weight):
+def liftingIndex (horizontialMulti, verticalMulti, DistMulti, cM, weight):
     #define the couplingMultiplier
     couplingMulti = couplingMultiplier(cM)
+    #print(couplingMulti)
+    dm = DMFactor(DistMulti)
+    hm = HMFactor(horizontialMulti)
+    vm = VMFactor(verticalMulti)
+    
     #define the recommended weight
-    rWL = RecommendWeight (horizontialMulti,verticalMulti,DistMulti, couplingMulti)
+    rWL = RecommendWeight (hm, vm, dm, float(couplingMulti))
     #index is Lifting weight index = weight / RWL
     index = weight / rWL
     
@@ -101,7 +106,7 @@ cm = input("Please enter the coupling Multiplier: ")
 weightOfObj = input("Please enter the weight of the object: ")
 
 #index is Lifting weight index = weight / RWL
-index = liftingIndex(float(hm), float(vm), float(dm), cm, int(weightOfObj))
+index = liftingIndex(int(hm), int(vm), int(dm), cm, int(weightOfObj))
 
 print(index)
 warnings(index)
