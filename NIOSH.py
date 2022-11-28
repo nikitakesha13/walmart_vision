c1736cb5b3f2775b3098f7a32a81c80ef1f86e16
annebell881 <83850788+annebell881@users.noreply.github.com>
2022-11-28 15:07:24 -0600

    Updated NIOSH GUI
    Needed the Cal File for the graph file

--- NIOSH.py
+++ NIOSH.py
@@ -1,5 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import NIOSH_graph
 from misc import *
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+
 
 #helper functions
 class Ui_nioshDialog(object):
@@ -18,6 +24,12 @@ class Ui_nioshDialog(object):
 		self.vUnit = _vUnit
 
 		self.gui = _gui
+
+		self.weight_arr = []
+		self.y_arr = []
+		self.good_risk = []
+		self.fair_risk = []
+		self.poor_risk = []
 
 	def getNIOSHIndex(self):
 		self.weight = self.weightBox.value()
@@ -60,23 +72,44 @@ class Ui_nioshDialog(object):
 		self.gui.label_2.setText("Object Weight (" + self.weightUnit.currentText() + "):")
 		self.gui.weightLabel.setText(str(self.weightBox.value()))
 
+	def setGraph(self):
+		#create the graphing object
+		self.graphingObj = NIOSH_graph.Graph(convertToInch(self.hDist, self.hUnit), convertToInch(self.vDist, self.vUnit), convertToInch(self.objDist, self.oUnit), "Good", convertToLb(self.weight, self.wUnit))
+		self.graphingObj.create_weight_array(self.weight_arr)
+		self.graphingObj.create_risk_array(self.good_risk, self.fair_risk, self.poor_risk, self.y_arr, self.weight_arr)
+        #set axis titles
+		self.graphwidget.setLabel('left', "<span style=\"color:white;font-size:15px\">NIOSH Risk Index</span>")
+		self.graphwidget.setLabel('bottom', "<span style=\"color:white;font-size:15px\">Weight of Box (lbs)</span>")
+        #add a legend
+		self.graphwidget.addLegend()
+        #create pens to edit the colors of the graph
+		pen_good = pg.mkPen(color=(0, 255, 0))
+		pen_fair = pg.mkPen(color=(0, 255, 0),style=QtCore.Qt.DashLine)
+		pen_poor = pg.mkPen(color=(0, 255, 0),style=QtCore.Qt.DotLine)
+		pen_high_risk = pg.mkPen(color=(255, 0, 0))
+        #Plot the lines on the graph
+		self.graphwidget.plot(self.weight_arr, self.good_risk, name = "Good Grip", pen = pen_good)
+		self.graphwidget.plot(self.weight_arr, self.fair_risk, name = "Fair Grip", pen = pen_fair)
+		self.graphwidget.plot(self.weight_arr, self.poor_risk, name = "Poor Grip", pen = pen_poor)
+		self.graphwidget.plot(self.weight_arr, self.y_arr, name = "High Risk", pen = pen_high_risk)
+
 	def setupUi(self, nioshDialog):
 		nioshDialog.setObjectName("nioshDialog")
-		nioshDialog.resize(380, 409)
+		nioshDialog.resize(814, 444)
 		nioshDialog.setAcceptDrops(False)
 		nioshDialog.setAutoFillBackground(False)
-		nioshDialog.setStyleSheet("background-color: rgb(44, 44, 44); color: #fff;")
+		nioshDialog.setStyleSheet("background-color: rgb(44, 44, 44);\n" "color: #fff;")
 		nioshDialog.setSizeGripEnabled(False)
 		nioshDialog.setModal(False)
 		
 		self.saveButton = QtWidgets.QPushButton(nioshDialog, clicked = lambda:self.getNIOSHIndex())
 		self.saveButton.setGeometry(QtCore.QRect(90, 370, 101, 30))
-		self.saveButton.setStyleSheet("background-color: white; color: black;")
+		self.saveButton.setStyleSheet("background-color: white;\n" "color: black;")
 		self.saveButton.setObjectName("saveButton")
 		
 		self.clearButton = QtWidgets.QPushButton(nioshDialog)
 		self.clearButton.setGeometry(QtCore.QRect(200, 370, 100, 30))
-		self.clearButton.setStyleSheet("background-color: white; color: black;")
+		self.clearButton.setStyleSheet("background-color: white;\n" "color: black;")
 		self.clearButton.setObjectName("clearButton")
 		
 		self.groupBox = QtWidgets.QGroupBox(nioshDialog)
@@ -92,7 +125,6 @@ class Ui_nioshDialog(object):
 		self.weightBox.setGeometry(QtCore.QRect(140, 30, 121, 21))
 		self.weightBox.setStyleSheet("border-color: rgb(44, 44, 44);")
 		self.weightBox.setObjectName("weightBox")
-		self.weightBox.setMaximum(1000.0)
 		
 		self.gripLabel = QtWidgets.QLabel(self.groupBox)
 		self.gripLabel.setGeometry(QtCore.QRect(10, 60, 71, 16))
@@ -100,7 +132,7 @@ class Ui_nioshDialog(object):
 		
 		self.gripBox = QtWidgets.QComboBox(self.groupBox)
 		self.gripBox.setGeometry(QtCore.QRect(140, 60, 181, 21))
-		self.gripBox.setStyleSheet("background-color: rgb(44, 44, 44); color: rgb(255, 255, 255);")
+		self.gripBox.setStyleSheet("background-color: rgb(44, 44, 44);\n" "color: rgb(255, 255, 255);")
 		self.gripBox.setMinimumContentsLength(3)
 		self.gripBox.setObjectName("gripBox")
 		self.gripBox.addItem("")
@@ -126,7 +158,6 @@ class Ui_nioshDialog(object):
 		self.freqBox.setStyleSheet("border-color: rgb(44, 44, 44);")
 		self.freqBox.setDecimals(0)
 		self.freqBox.setObjectName("freqBox")
-		self.freqBox.setMaximum(100)
 		
 		self.label = QtWidgets.QLabel(self.groupBox_2)
 		self.label.setGeometry(QtCore.QRect(210, 30, 31, 16))
@@ -146,12 +177,10 @@ class Ui_nioshDialog(object):
 		self.objDistLabel = QtWidgets.QLabel(self.groupBox_3)
 		self.objDistLabel.setGeometry(QtCore.QRect(10, 30, 91, 20))
 		self.objDistLabel.setObjectName("objDistLabel")
-		
 		self.objDistBox = QtWidgets.QDoubleSpinBox(self.groupBox_3)
 		self.objDistBox.setGeometry(QtCore.QRect(140, 30, 121, 21))
 		self.objDistBox.setStyleSheet("border-color: rgb(44, 44, 44);")
 		self.objDistBox.setObjectName("objDistBox")
-		self.objDistBox.setMaximum(1000.0)
 		
 		self.hdLabel = QtWidgets.QLabel(self.groupBox_3)
 		self.hdLabel.setGeometry(QtCore.QRect(10, 60, 121, 16))
@@ -161,13 +190,11 @@ class Ui_nioshDialog(object):
 		self.hDistanceBox.setGeometry(QtCore.QRect(140, 60, 121, 21))
 		self.hDistanceBox.setStyleSheet("border-color: rgb(44, 44, 44);")
 		self.hDistanceBox.setObjectName("hDistanceBox")
-		self.hDistanceBox.setMaximum(1000.0)
 		
 		self.vDistanceBox = QtWidgets.QDoubleSpinBox(self.groupBox_3)
 		self.vDistanceBox.setGeometry(QtCore.QRect(140, 90, 121, 21))
 		self.vDistanceBox.setStyleSheet("border-color: rgb(44, 44, 44);")
 		self.vDistanceBox.setObjectName("vDistanceBox")
-		self.vDistanceBox.setMaximum(1000.0)
 		
 		self.vdLabel = QtWidgets.QLabel(self.groupBox_3)
 		self.vdLabel.setGeometry(QtCore.QRect(10, 90, 121, 16))
@@ -184,9 +211,6 @@ class Ui_nioshDialog(object):
 		self.hDistUnit = QtWidgets.QComboBox(self.groupBox_3)
 		self.hDistUnit.setGeometry(QtCore.QRect(270, 60, 51, 22))
 		self.hDistUnit.setObjectName("hDistUnit")
-		
-		self.hDistUnit.addItem("")
-		self.hDistUnit.addItem("")
 		self.hDistUnit.addItem("")
 		self.hDistUnit.addItem("")
 		
@@ -195,27 +219,42 @@ class Ui_nioshDialog(object):
 		self.vDistUnit.setObjectName("vDistUnit")
 		self.vDistUnit.addItem("")
 		self.vDistUnit.addItem("")
-		self.vDistUnit.addItem("")
-		self.vDistUnit.addItem("")
-
+		
+		self.graphwidget = PlotWidget(nioshDialog)
+		self.graphwidget.setGeometry(QtCore.QRect(409, 39, 371, 321))
+		self.graphwidget.setObjectName("graphwidget")
+		
+		self.plot = QtWidgets.QPushButton(nioshDialog, clicked = lambda:self.setGraph())
+		self.plot.setGeometry(QtCore.QRect(520, 370, 151, 30))
+		self.plot.setStyleSheet("background-color: white;\n""color: black;")
+		self.plot.setObjectName("plot")
+		
 		self.retranslateUi(nioshDialog)
 		QtCore.QMetaObject.connectSlotsByName(nioshDialog)
 
 	def retranslateUi(self, nioshDialog):
 		_translate = QtCore.QCoreApplication.translate
 		nioshDialog.setWindowTitle(_translate("nioshDialog", "NIOSH Calculator"))
+		
 		self.saveButton.setText(_translate("nioshDialog", "Save"))
 		self.clearButton.setText(_translate("nioshDialog", "Reset"))
+		
 		self.groupBox.setTitle(_translate("nioshDialog", "General"))
+		
 		self.weightLabel.setText(_translate("nioshDialog", "Object Weight"))
+		
 		self.gripLabel.setText(_translate("nioshDialog", "Grip Quality"))
 		self.gripBox.setItemText(0, _translate("nioshDialog", "Good"))
 		self.gripBox.setItemText(1, _translate("nioshDialog", "Fair"))
-		self.gripBox.setItemText(2, _translate("nioshDialog", "Poor"))
+		self.gripBox.setItemText(2, _translate("nioshDialog", "Bad"))
+		
 		self.weightUnit.setItemText(0, _translate("nioshDialog", "lb"))
 		self.weightUnit.setItemText(1, _translate("nioshDialog", "kg"))
+		
 		self.groupBox_2.setTitle(_translate("nioshDialog", "Advanced"))
+		
 		self.freqLabel.setText(_translate("nioshDialog", "Frequency"))
+		
 		self.label.setText(_translate("nioshDialog", "per"))
 		
 		self.freqUnit.setItemText(0, _translate("nioshDialog", "second"))
@@ -223,7 +262,9 @@ class Ui_nioshDialog(object):
 		self.freqUnit.setItemText(2, _translate("nioshDialog", "hour"))
 		
 		self.groupBox_3.setTitle(_translate("nioshDialog", "NIOSH Risk Index"))
+		
 		self.objDistLabel.setText(_translate("nioshDialog", "Object Distance"))
+		
 		self.hdLabel.setText(_translate("nioshDialog", "Horizontal Distnance"))
 		self.vdLabel.setText(_translate("nioshDialog", "Vertical Distance"))
 		
@@ -232,15 +273,13 @@ class Ui_nioshDialog(object):
 		self.objDistUnit.setItemText(2, _translate("nioshDialog", "cm"))
 		self.objDistUnit.setItemText(3, _translate("nioshDialog", "m"))
 		
-		self.hDistUnit.setItemText(0, _translate("nioshDialog", "in"))
-		self.hDistUnit.setItemText(1, _translate("nioshDialog", "ft"))
-		self.hDistUnit.setItemText(2, _translate("nioshDialog", "cm"))
-		self.hDistUnit.setItemText(3, _translate("nioshDialog", "m"))
-
-		self.vDistUnit.setItemText(0, _translate("nioshDialog", "in"))
-		self.vDistUnit.setItemText(1, _translate("nioshDialog", "ft"))
-		self.vDistUnit.setItemText(2, _translate("nioshDialog", "cm"))
-		self.vDistUnit.setItemText(3, _translate("nioshDialog", "m"))
+		self.hDistUnit.setItemText(0, _translate("nioshDialog", "lb"))
+		self.hDistUnit.setItemText(1, _translate("nioshDialog", "kg"))
+		
+		self.vDistUnit.setItemText(0, _translate("nioshDialog", "lb"))
+		self.vDistUnit.setItemText(1, _translate("nioshDialog", "kg"))
+		
+		self.plot.setText(_translate("nioshDialog", "Plot"))
 
 #create a class for the NIOSH equation
 class Calc:
